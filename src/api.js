6a8185a7560c3677708ecc6f8ebdadd075165210
Dabbett <abbettdylan@gmail.com>
2023-12-15 12:37:00 -0500

    first commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+// const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9390205777msh048d388c288bcbfp11ce14jsnd94d88d4c77b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "7c7eaf40ebe5a36efa72120d791e34d9";
